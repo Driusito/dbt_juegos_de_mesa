7beec7169d78c6d40de0f14ff761aa2dc53e8e8e
Andres Jurado <driujuegos@gmail.com>
2026-05-17 23:04:59 +0000

    user_sk

--- models/marts/fct_reviews.sql
+++ models/marts/fct_reviews.sql
@@ -19,7 +19,6 @@ dim_game as (
 dim_user as (
 
     select * from {{ ref('dim_user') }}
-    where is_current = true
 
 ),
 
@@ -36,23 +35,22 @@ final as (
         u.user_sk,
         g.game_sk,
         d.date_sk,
-        case 
-        when r.rating > 10.0
-            then 10
-        when r.rating < 0
-            then 0
-        else 
-            r.rating
-        end as rating,
+        case
+            when r.rating > 10.0 then 10
+            when r.rating < 0    then 0
+            else r.rating
+        end                                          as rating,
         coalesce(r.helpful_votes, 0)                 as helpful_votes,
         len(r.review_text)                           as review_length_chars,
         r.review_date,
         r._loaded_at
 
     from stg_reviews      r
-    left join dim_game    g on r.game_id     = g.game_id
-    left join dim_user    u on r.user_id     = u.user_id
-    left join dim_date    d on r.review_date = d.date
+    left join dim_game    g on r.game_id      = g.game_id
+    left join dim_user    u on r.user_id      = u.user_id
+                           and r.review_date >= u.valid_from
+                           and r.review_date <  u.valid_to
+    left join dim_date    d on r.review_date  = d.date
 
     where g.game_sk is not null
       and u.user_sk is not null
