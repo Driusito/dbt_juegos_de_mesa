fa3ed47272610ec6c7242d263637862d352c275b
Andres Jurado <driujuegos@gmail.com>
2026-05-19 21:51:43 +0000

    orphan users

--- models/marts/dim_user.sql
+++ models/marts/dim_user.sql
@@ -62,7 +62,6 @@ enriched as (
 
 missing_users as (
 
-    -- Usuarios que aparecen en FCT pero NO existen en base
     select distinct user_id
     from (
         select regexp_replace(lower(trim(user_id)), '[^a-z0-9-]', '') as user_id
